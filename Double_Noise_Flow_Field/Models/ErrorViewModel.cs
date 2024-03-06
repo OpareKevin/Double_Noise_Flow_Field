9de7b5aed80d41d696935ba73ec1292b977ea80f
kev <oparekevin@gmail.com>
2024-03-06 14:25:27 +0000

    Initial commit

---
+++ Double_Noise_Flow_Field/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Double_Noise_Flow_Field.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
